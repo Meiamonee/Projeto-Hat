dff707f445affeb66e800b714fe2550f4fc902d7
Pedro Lucas de Souza Faria <105456789+Meiamonee@users.noreply.github.com>
2025-09-05 01:52:40 -0300

    pequenas mudanças

--- HAT/src/Componentes/BackgroundVideo/BackgroundVideo.jsx
+++ HAT/src/Componentes/BackgroundVideo/BackgroundVideo.jsx
@@ -1,19 +1,10 @@
 export default function BackgroundVideo() {
   return (
     <div className="fixed inset-0 z-[-1] overflow-hidden">
-      {/* Vídeo */}
-      <video
-        className="w-full h-full object-cover"
-        autoPlay
-        loop
-        muted
-        playsInline
-      >
+      <video className="w-full h-full object-cover" autoPlay loop muted playsInline>
         <source src="/Video/videoplayback.mp4" type="video/mp4" />
         Seu navegador não suporta vídeo.
       </video>
-
-      {/* Overlay com gradiente */}
       <div className="absolute inset-0 bg-gradient-to-r from-black via-black/80 to-black"></div>
     </div>
   )
